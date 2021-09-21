b939d85da627f9653cf45cc6d727b8da75c19224
Msarate16 <89851155+Msarate16@users.noreply.github.com>
2021-09-21 07:17:59 -0500

    Add files via upload

---
+++ Scripts_Python/suma.py
@@ -0,0 +1,12 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10
+>>> b=20
+>>> suma = a+b
+>>> print (suma)
+30
+>>> print("La suma es: ", suma)
+La suma es:  30
+>>> print ("La suma de ", a, "mas ", b , "es", suma)
+La suma de  10 mas  20 es 30
+>>> 
