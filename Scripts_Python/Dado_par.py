4d680c07f475bd6a7eb7a9d57fca2dc2042a0ca9
Msarate16 <89851155+Msarate16@users.noreply.github.com>
2021-09-21 07:20:43 -0500

    Scripts de python

---
+++ Scripts_Python/Dado_par.py
@@ -0,0 +1,20 @@
+import os
+from random import randint
+
+
+def dices():
+    status =True
+
+    while status:
+        dice1 = randint(1,6)
+        dice2 = randint(1,6)
+        print("Dado 1: ", dice1)
+        print("Dado 2: ", dice2)
+
+        if dice1 == dice2:
+            status = False
+            print("::: Los dados son pares. El lanzamiento ha finalizado::: ")
+        else :
+            key = input("::: Presiona cualquier tecla para lanzar los dados nuevamente :::")
+
+dices()
---
+++ Scripts_Python/calculadora.py
@@ -0,0 +1,12 @@
+#calculadora basica
+a=10
+b=5
+suma=a+b
+resta=a-b
+multiplicacion=a*b
+division=a/b
+
+print ("La suma es :", suma)
+print ("La resta es :", resta)
+print ("La multiplicacion es :", multiplicacion)
+print ("La division es :", division)
